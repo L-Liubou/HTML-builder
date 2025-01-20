5eb7e094d492cd49abb5a5560766c1190bcbd429
Liubou Levitskaya <lyubasaraeva@gmail.com>
2025-01-20 18:41:39 +0300

    feat: add colors to console output for better visibility

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -3,9 +3,19 @@ const path = require('path');
 
 const folderPath = path.join(__dirname, 'secret-folder');
 
+const colors = {
+  yellow: '\x1b[93m',
+  blue: '\x1b[94m',
+  white: '\x1b[97m',
+  red: '\x1b[31m',
+  reset: '\x1b[0m',
+};
+
 fs.readdir(folderPath, { withFileTypes: true }, (err, files) => {
   if (err) {
-    console.error(`Error reading a folder: ${err.message}`);
+    console.error(
+      `${colors.red}Error reading a folder: ${err.message}${colors.reset}`,
+    );
     return;
   }
 
@@ -15,12 +25,13 @@ fs.readdir(folderPath, { withFileTypes: true }, (err, files) => {
       const { name, ext } = path.parse(file.name);
       fs.stat(filePath, (err, stats) => {
         if (err) {
-          console.error(`Error in receiving information: ${err.message}`);
+          console.error(
+            `${colors.red}Error in receiving information: ${err.message}${colors.reset}`,
+          );
           return;
         }
         const fileSize = (stats.size / 1024).toFixed(2);
-        console.log(`${name} - ${ext.slice(1)} - ${fileSize}kb`);
-      });
+        console.log(`${colors.yellow}${name}${colors.reset} - ${colors.blue}${ext.slice(1)}${colors.reset} - ${colors.white}${fileSize}kb${colors.reset}`);});
     }
   });
 });
