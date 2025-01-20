e4dadde644eef529d537168c0b51a00ca2fa057a
Liubou Levitskaya <lyubasaraeva@gmail.com>
2025-01-20 18:02:28 +0300

    feat: add handling for empty file in stream processing

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -4,7 +4,19 @@ const path = require('path');
 const filePath = path.join(__dirname, 'text.txt');
 const stream = fs.createReadStream(filePath, 'utf-8');
 
-stream.on('data', (chunk) => process.stdout.write(chunk));
+let isEmpty = true;
+
+stream.on('data', (chunk) => {
+  isEmpty = false;
+  process.stdout.write(chunk);
+});
+
+stream.on('end', () => {
+  if (isEmpty) {
+    console.log('\x1b[90mThe file is empty now\x1b[0m');
+  }
+});
+
 stream.on('error', (err) => {
   console.error('\x1b[91mError reading file:\x1b[0m', err.message);
 });
