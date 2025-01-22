2943671fa2e16968de7052317a2d76aaa4f01b9c
Liubou Levitskaya <lyubasaraeva@gmail.com>
2025-01-22 13:40:09 +0300

    fix: use fs.promises for async directory access

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,24 +1,26 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
 
 const oldDirPath = path.join(__dirname, 'files');
 const newDirPath = path.join(__dirname, 'files-copy');
 
 async function copyDir(oldDir, newDir) {
-  if (!fs.existsSync(oldDir)) {
+  try {
+    await fs.access(oldDir);
+  } catch {
     console.error('\x1b[91mCan not find "files" directory\x1b[0m');
     return;
   }
   try {
-    await fs.promises.rm(newDir, { recursive: true, force: true });
-    await fs.promises.mkdir(newDir, { recursive: true });
-    const files = await fs.promises.readdir(oldDir, { withFileTypes: true });
+    await fs.rm(newDir, { recursive: true, force: true });
+    await fs.mkdir(newDir, { recursive: true });
+    const files = await fs.readdir(oldDir, { withFileTypes: true });
 
     for (const file of files) {
       const oldFilePath = path.join(oldDir, file.name);
       const newFilePath = path.join(newDir, file.name);
       if (file.isFile()) {
-        await fs.promises.copyFile(oldFilePath, newFilePath);
+        await fs.copyFile(oldFilePath, newFilePath);
       }
     }
     console.log('\x1b[92mThe folder was successfully copied/update\x1b[0m');
